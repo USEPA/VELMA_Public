e9eccacc820579eb9b8ca2fb9ff66820c38ac9e0
Travis Hudson <Hudson.Travis@epa.gov>
2024-04-29 11:07:31 -0700

    Finished VELMA 2.0 manual conversion

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # New VELMA 2.2 Documentation Branch
 
-Once given access run the following 
+Process
 
-```
-git push -u origin documentation
-```
+- First convert pdfs for Velma 2.0 and 2.1 into HTML
+- Convert important content to Pug template
+- Served with Express
 
-Notes: 
+Notes:
 
 - Modern UI components: [atherosai/ui: Simple UI examples from my social media (github.com)](https://github.com/atherosai/ui/tree/main)
--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const port = 3000
 
 // helpful link https://codebeautify.org/html-to-pug-converter
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
+  res.render('index', { pageTitle: 'Home' })
+})
+
+app.get('/intro', (req, res) => {
     res.render('intro', { pageTitle: 'Intro' })
   })
 
@@ -25,12 +31,44 @@ app.get('/files', (req, res) => {
   res.render('input_files', { pageTitle: 'Input Files'})
 })
 
+// app.get('/file-details', (req, res) => {
+//   res.render('input_files_details', { pageTitle: 'Input File Details'})
+// })
+
 app.get('/sim-config', (req, res) => {
   res.render('new_sim_config', { pageTitle: 'New Sim Config'})
 })
 
 app.get('/param-config', (req, res) => {
-  res.render('parameter_config', { pageTitle: 'New Sim Config'})
+  res.render('parameter_config', { pageTitle: 'Parameter Config'})
+})
+
+app.get('/appendix-1', (req, res) => {
+  res.render('appendix_1', { pageTitle: 'Appendix 1'})
+})
+
+app.get('/appendix-2', (req, res) => {
+  res.render('appendix_2', { pageTitle: 'Appendix 2'})
+})
+
+app.get('/appendix-3', (req, res) => {
+  res.render('appendix_3', { pageTitle: 'Appendix 3'})
+})
+
+app.get('/appendix-4', (req, res) => {
+  res.render('appendix_4', { pageTitle: 'Appendix 4'})
+})
+
+app.get('/appendix-5', (req, res) => {
+  res.render('appendix_5', { pageTitle: 'Appendix 5'})
+})
+
+app.get('/appendix-6', (req, res) => {
+  res.render('appendix_6', { pageTitle: 'Appendix 6'})
+})
+
+app.get('/appendix-7', (req, res) => {
+  res.render('appendix_7', { pageTitle: 'Appendix 7'})
 })
 // use this as an example https://github.com/expressjs/express/blob/master/examples/ejs/index.js
 // already have express and pug installed just need to go from there.
