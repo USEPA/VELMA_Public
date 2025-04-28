2002674d79af28c1307ff5b483c140ea44838435
Travis Hudson <Hudson.Travis@epa.gov>
2025-04-28 13:10:43 -0700

    Finished current iteration of reorganization for versions 2.0, 2.1, and 2.2

--- routes/2_0.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-
-const router = express.Router()
-
-router.get('/about', (req, res, next) => {
-    res.render('about', {pageTitle: 'about'})
-})
-
-module.exports = router
